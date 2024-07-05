e284e59703d762f568153760b610a327436eff05
esonusharma <esonusharma@gmail.com>
2024-07-05 19:49:12 +0530

    update

--- theme/calculators/home.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-st.title("esonusharma Calculators")
-st.text('This app hosts multiple calculators of esonusharma.github.io')
-st.text('This code/calculator/service/product is subject to the terms of the MIT License')
